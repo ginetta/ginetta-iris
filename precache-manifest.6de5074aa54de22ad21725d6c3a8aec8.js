c42912661186d8c3b793873ae36dd17775526ac3
Bruno Lourenco <brunnolou@gmail.com>
2019-04-03 18:39:04 +0100

    Updates

---
+++ precache-manifest.6de5074aa54de22ad21725d6c3a8aec8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a382c3e7d0ae8e24bf26",
+    "url": "/ginetta-iris/static/js/runtime~main.e547ea36.js"
+  },
+  {
+    "revision": "ee890d1be0bb3eeac3ea",
+    "url": "/ginetta-iris/static/js/main.a3af770d.chunk.js"
+  },
+  {
+    "revision": "909d80f10163cbb24db2",
+    "url": "/ginetta-iris/static/js/2.24de1fad.chunk.js"
+  },
+  {
+    "revision": "ee890d1be0bb3eeac3ea",
+    "url": "/ginetta-iris/static/css/main.07755ae5.chunk.css"
+  },
+  {
+    "revision": "93af4237906e7497b401a1ac0245dbaa",
+    "url": "/ginetta-iris/index.html"
+  }
+];
